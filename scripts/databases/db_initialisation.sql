9a258e20197405606dcc042f89f1a7d4d3f90e23
bfx <francois.xavier.brandao@gmail.com>
2023-06-28 07:46:01 +0000

    Basic script for database initialisation

---
+++ scripts/databases/db_initialisation.sql
@@ -0,0 +1,24 @@
+--
+--
+-- Script of initialisation of the database
+-- Version: 1.0
+--
+--
+
+CREATE TABLE USER {
+       user_id INT PRIMARY KEY NOT NULL,
+       user_email VARCHAR(255) NOT NULL,
+       user_password VARCHAR(255) NOT NULL,
+       user_type INT NOT NULL,
+       user_recover VARCHAR(16),
+       UNIQUE(user_recover)
+}
+
+-- Example of data insertion
+-- INSERT INTO USER(user_email, user_password, user_type) VALUES ('example@gmail.com', '', 0);
+
+-- Example of data updating
+-- UPDATE USER SET user_password = '' WHERE user_id = ;
+
+-- Example of data recovering
+-- SELECT user_id, user_email, user_type FROM USER WHERE user_email = '' AND user_password = '';
